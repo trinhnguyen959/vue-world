65520458dc9e9c9b8e0950ba891f418af7368e43
trinhnv <trinhnguyen959@gmail.com>
2022-05-10 09:23:08 +0700

    v-for and binding data

--- rhp-team/events/main.js
+++ rhp-team/events/main.js
@@ -1,20 +1,47 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			x: 0,
-			y: 0,
+			testClass: 'flex',
+			products: [
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product a',
+					price: 120.000
+				},
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product b',
+					price: 120.000
+				},
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product c',
+					price: 120.000
+				},
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product d',
+					price: 120.000
+				},
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product e',
+					price: 120.000
+				},
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product f',
+					price: 120.000
+				},
+				{
+					thumb: './assets/favicon.ico',
+					name: 'product g',
+					price: 120.000
+				},
+			]
 		};
 	},
 	methods: {
-		onLogEvent(event, newX, newY) {
-			console.log(event);
-			this.x = newX;
-			this.y = newY;
-		},
-		onMouseMoveEvent(event) {
-			this.x = event.offsetX;
-			this.y = event.offsetY;
-		},
 	},
 });
 
