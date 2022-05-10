f744e46fd31a80fb1decde97117645a43ecbe489
trinhnv <trinhnguyen959@gmail.com>
2022-05-10 10:58:44 +0700

    binding data

--- rhp-team/events/main.js
+++ rhp-team/events/main.js
@@ -6,42 +6,52 @@ const app = Vue.createApp({
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product a',
-					price: 120.000
+					price: 120.000,
+					isInCard: false
 				},
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product b',
-					price: 120.000
+					price: 120.000,
+					isInCard: false
 				},
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product c',
-					price: 120.000
+					price: 120.000,
+					isInCard: false
 				},
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product d',
-					price: 120.000
+					price: 120.000,
+					isInCard: true
 				},
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product e',
-					price: 120.000
+					price: 120.000,
+					isInCard: true
 				},
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product f',
-					price: 120.000
+					price: 120.000,
+					isInCard: false
 				},
 				{
 					thumb: './assets/favicon.ico',
 					name: 'product g',
-					price: 120.000
+					price: 120.000,
+					isInCard: true
 				},
 			]
 		};
 	},
 	methods: {
+		onToggleCart(event, product) {
+			product.isInCard = !product.isInCard;
+		},
 	},
 });
 
