357f0791f19de61f765e1d8c2fe2902f4e6a6b23
trinhnv <trinhnguyen959@gmail.com>
2022-05-19 06:33:08 +0700

    add quiz form

---
+++ rhp-team/quiz-form/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <quiz-form />
+</template>
+
+<script>
+import QuizForm from "./components/QuizForm.vue";
+export default {
+  name: "App",
+  components: {
+    QuizForm,
+  },
+};
+</script>
